7cd010d96528b661c1b0a7333d53a774cffb76b8
gilgil <gilgil@gilgil.net>
2019-05-26 21:10:09 +0900

    Add div(1000000) parameter and change a few codes

--- src/gtimediff.h
+++ src/gtimediff.h
@@ -25,43 +25,43 @@
  *
  * For more information, see GTimeDiffNull
  */
-template <typename MILESTONE, typename CLOCK, typename DIFF, class TIMER>
+template <typename MILESTONE, typename CLOCK, typename DIFF, typename TIMER>
 class GTimeDiff {
   struct Key {
-    MILESTONE from;
-    MILESTONE to;
+    MILESTONE from_;
+    MILESTONE to_;
 
     bool operator < (const Key& rhs) const {
-      if (this->from < rhs.from) return true;
-      if (this->from > rhs.from) return false;
-      if (this->to < rhs.to) return true;
+      if (this->from_ < rhs.from_) return true;
+      if (this->from_ > rhs.from_) return false;
+      if (this->to_ < rhs.to_) return true;
       return false;
     }
 
     bool operator == (const Key& rhs) const {
-      return (from == rhs.from) && (to == rhs.to);
+      return (from_ == rhs.from_) && (to_ == rhs.to_);
     }
 
     struct Hash {
       size_t operator()(const Key &key) const {
-        return std::hash<MILESTONE>()(key.from) ^ std::hash<MILESTONE>()(key.to);
+        return std::hash<MILESTONE>()(key.from_) ^ std::hash<MILESTONE>()(key.to_);
       }
     };
   };
 
   struct Data {
   public:
-    int count;
-    DIFF totalElapsed;
+    int count_;
+    DIFF totalElapsed_;
   };
 
   typedef std::map<Key, Data> ReportMap;
 
 public:
-  TIMER timer;
-  ReportMap reportMap;
-  MILESTONE lastMilestone;
-  CLOCK lastClock;
+  TIMER timer_;
+  ReportMap reportMap_;
+  MILESTONE lastMilestone_;
+  CLOCK lastClock_;
 
 public:
   GTimeDiff(MILESTONE startMilestone = 0) {
@@ -71,48 +71,48 @@ public:
   }
 
   virtual ~GTimeDiff() {
-    if (reportMap.size() > 0)
+    if (reportMap_.size() > 0)
       report(true);
   }
 
   void clear() {
-    reportMap.clear();
-    lastMilestone = 0;
-    lastClock = timer.now();
+    reportMap_.clear();
+    lastMilestone_ = 0;
+    lastClock_ = timer_.now();
   }
 
   void check(MILESTONE milestone) {
-    check(milestone, timer.now());
+    check(milestone, timer_.now());
   }
 
   void check(MILESTONE milestone, CLOCK now) {
     Key key;
-    key.from = lastMilestone;
-    key.to = milestone;
+    key.from_ = lastMilestone_;
+    key.to_ = milestone;
 
-    Data &data = reportMap[key];
-    data.count++;
-    data.totalElapsed += DIFF(now - lastClock);
+    Data &data = reportMap_[key];
+    data.count_++;
+    data.totalElapsed_ += DIFF(now - lastClock_);
 
-    lastMilestone = milestone;
-    lastClock = now;
+    lastMilestone_ = milestone;
+    lastClock_ = now;
   }
 
-  virtual void report(bool autoClear = true, std::ostream* os = nullptr) {
+  virtual void report(bool autoClear = true, int div = 1000000, std::ostream* os = nullptr) {
     if (os == nullptr) os = &std::cout;
-    os->imbue(std::locale(""));
-    *os << "beg\tend\tcount\telapsed\taverage\n";
-    for (auto it = reportMap.begin(); it != reportMap.end(); it++) {
+    *os << "beg\t\tend\t\tcount\t\telapsed\t\taverage\n";
+    for (auto it = reportMap_.begin(); it != reportMap_.end(); it++) {
       Key key = it->first;
-      if (key.from == 0) continue;
+      if (key.from_ == 0) continue;
       Data data = it->second;
-      assert(data.count != 0);
-      DIFF avg = data.totalElapsed / data.count;
-      *os << key.from
-        << "\t" << key.to
-        << "\t" << data.count
-        << "\t" << data.totalElapsed
-        << "\t" << avg
+      assert(data.count_ != 0);
+      DIFF elapsed = data.totalElapsed_ / div;
+      DIFF avg = elapsed / data.count_;
+      *os << key.from_
+        << "\t\t" << key.to_
+        << "\t\t" << data.count_
+        << "\t\t" << elapsed
+        << "\t\t" << avg
         << std::endl;
     }
     if (autoClear) clear();
